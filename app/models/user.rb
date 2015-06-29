8c938db3ccdc28bb00cc264d2738475d0707d9c0
crisefd <cristhian.fuertes@correounivalle.edu.co>
2015-06-29 21:46:36 +0000

    basic user model complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    before_save { self.email = email.downcase }
+    validates :name, presence: true, length: { maximum: 50, minimum: 3 }
+    validates :email, presence: true, length: { maximum: 255, minimum: 7 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+    validates :password, presence: true, length: { minimum: 6 }
+    has_secure_password
 end
