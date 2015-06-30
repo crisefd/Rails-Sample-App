d7962cddee73d1363907c89bacf3aca9ae0cf911
crisefd <cristhian.fuertes@correounivalle.edu.co>
2015-06-30 01:07:10 +0000

    Adding valid email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    before_save { self.email = email.downcase }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    before_save { email.downcase! }
     validates :name, presence: true, length: { maximum: 50, minimum: 3 }
     validates :email, presence: true, length: { maximum: 255, minimum: 7 },
                     format: { with: VALID_EMAIL_REGEX },
