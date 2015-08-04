344df43a76d8fc92a4db26b391bae2d92cf71d28
crisefd <cristhian.fuertes@correounivalle.edu.co>
2015-08-04 00:22:17 +0000

    Account activation resource

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+  
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
